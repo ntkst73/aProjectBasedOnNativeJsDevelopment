298c0054723c576ee249b4d621b066b2ecfd43f9
Кошарный Никита <ntkst73@mail.ru>
2020-10-22 17:48:17 +0400

    Second Array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import './styles/style.css'
+import './styles/style.css';
 //стили
 
-import image from './images/img.gif'
-import image1 from './images/magnify.png'
+import image from './images/img.gif';
+import image1 from './images/magnify.png';
 //картинки
+
+let linkMenu = document.querySelectorAll('.js-menu-item');
+
+
+let objLink = {
+    name1: 'main',
+    name2: 'services',
+    name3: 'product',
+    name4: 'about',
+    name5: 'contact'
+};
+
+
+var  objKeys = Object.keys(objLink);
+//берем ключи
+
+linkMenu.forEach(function (item, index) {
+    var oneName = objLink[objKeys[index]];
+    item.innerText = oneName;
+});
