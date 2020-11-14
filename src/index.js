0a67394b36e9bcfc44abbf399d6c041c760d35c0
Кошарный Никита <ntkst73@mail.ru>
2020-11-14 15:49:24 +0400

    rest and depf par

--- src/index.js
+++ src/index.js
@@ -118,12 +118,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Блок наше меню за день
     class MenuCard {
-        constructor(src, alt , title, desc, price, parentSelector) {
+        constructor(src, alt , title, desc, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.desc = desc;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAN();
@@ -136,8 +137,14 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
             element.innerHTML = `
-            <div class="menu__item">
                 <img src="${this.src}" alt="${this.alt}">
                 <h3 class="menu__item-subtitle">${this.title}"</h3>
                 <div class="menu__item-descr">${this.desc}</div>
@@ -146,7 +153,6 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div> 
-            </div>
             `;
             this.parent.append(element);
         }
